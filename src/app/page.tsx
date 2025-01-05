e1ba08b7bde802007ca0030899c8c003767f0ba6
victorcamargo <victoryuritc@yahoo.com>
2025-01-05 05:26:48 -0300

    feat: complete interactive home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,44 @@
 "use client";
 
 import PageTemplate from "@/components/atomic/templates/page-template";
+import AllHomePageLightsabers from "@/components/pages/all-home-page-lightsabers";
 import { useAppDispatch } from "@/redux/hooks";
 import {
   setIsBackgroundMusicEnabled,
   setHasUserStartedTheJourney,
 } from "@/redux/slices/user-preferences";
 import { useRouter } from "next/navigation";
+import { Dispatch, SetStateAction, useRef, useState } from "react";
 
 export default function HomePage() {
+  const [
+    shouldShowAllBlurredRoundedLights,
+    setShouldShowAllBlurredRoundedLights,
+  ] = useState(false);
+  const [shouldShowRedLightsaber, setShouldShowRedLightsaber] = useState(false);
+  const [shouldShowGreenLightsaber, setShouldShowGreenLightsaber] =
+    useState(false);
+
   return (
     <PageTemplate shouldHideDefaultMarginY>
       <div className="min-h-screen flex flex-col items-center justify-center gap-y-10">
+        <AllHomePageLightsabers
+          shouldShowAllBlurredRoundedLights={shouldShowAllBlurredRoundedLights}
+          shouldShowGreenLightsaber={shouldShowGreenLightsaber}
+          shouldShowRedLightsaber={shouldShowRedLightsaber}
+        />
+
         <HomePageTitle />
 
         <HomePageDescription />
 
-        <HomePageStartJouneyButton />
+        <HomePageStartJouneyButton
+          setShouldShowAllBlurredRoundedLights={
+            setShouldShowAllBlurredRoundedLights
+          }
+          setShouldShowGreenLightsaber={setShouldShowGreenLightsaber}
+          setShouldShowRedLightsaber={setShouldShowRedLightsaber}
+        />
       </div>
     </PageTemplate>
   );
@@ -40,16 +62,50 @@ function HomePageDescription() {
   );
 }
 
-function HomePageStartJouneyButton() {
+type HomePageStartJouneyButtonProps = {
+  setShouldShowAllBlurredRoundedLights: Dispatch<SetStateAction<boolean>>;
+  setShouldShowRedLightsaber: Dispatch<SetStateAction<boolean>>;
+  setShouldShowGreenLightsaber: Dispatch<SetStateAction<boolean>>;
+};
+
+function HomePageStartJouneyButton(props: HomePageStartJouneyButtonProps) {
+  const greenLightsaberAudioRef = useRef<HTMLAudioElement>(null);
+  const redLightsaberAudioRef = useRef<HTMLAudioElement>(null);
+  const journeyHasStartedAudioRef = useRef<HTMLAudioElement>(null);
+
   const dispatch = useAppDispatch();
 
   const router = useRouter();
 
   async function handleOnClickSetHasUserStartedTheJourney() {
-    dispatch(setHasUserStartedTheJourney(true));
-    dispatch(setIsBackgroundMusicEnabled(true));
+    props.setShouldShowGreenLightsaber(true);
 
-    router.push("/characters");
+    if (greenLightsaberAudioRef.current) {
+      greenLightsaberAudioRef.current.play();
+    }
+
+    setTimeout(() => {
+      props.setShouldShowRedLightsaber(true);
+
+      if (redLightsaberAudioRef.current) {
+        redLightsaberAudioRef.current.play();
+      }
+    }, 750);
+
+    setTimeout(() => {
+      props.setShouldShowAllBlurredRoundedLights(true);
+
+      if (journeyHasStartedAudioRef.current) {
+        journeyHasStartedAudioRef.current?.play();
+      }
+    }, 1500);
+
+    setTimeout(() => {
+      dispatch(setHasUserStartedTheJourney(true));
+      dispatch(setIsBackgroundMusicEnabled(true));
+
+      router.push("/characters");
+    }, 2500);
   }
 
   return (
@@ -57,6 +113,10 @@ function HomePageStartJouneyButton() {
       onClick={handleOnClickSetHasUserStartedTheJourney}
       className="rounded-full py-2 px-6 bg-white hover:bg-gray-300">
       <span className="text-black text-lg font-medium">Start Journey</span>
+
+      <audio ref={greenLightsaberAudioRef} src={`/lightsaber-1.mp3`} />
+      <audio ref={redLightsaberAudioRef} src={`/lightsaber-2.mp3`} />
+      <audio ref={journeyHasStartedAudioRef} src={`/lightsaber-3.mp3`} />
     </button>
   );
 }
